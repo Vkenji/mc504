25d7a2743e00ea63c472e5c03721727991bdfe47
Vkenji <vitor95uema@hotmail.com>
2016-09-20 15:01:44 -0300

    Initial Commit

---
+++ mc504/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mc504
+//
+//  Created by Vitor Uema on 9/20/16.
+//  Copyright © 2016 Vitor Uema. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
