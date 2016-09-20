0a6655e95f496ec7377a12ee1c26899b4eefb1a3
Vkenji <vitor95uema@hotmail.com>
2016-09-20 18:20:20 -0300

    inicio do projeto

---
+++ mc504/projeto_multithread/immigrants.h
@@ -0,0 +1,13 @@
+//
+//  immigrants.h
+//  mc504
+//
+//  Created by Vitor Uema on 9/20/16.
+//  Copyright © 2016 Vitor Uema. All rights reserved.
+//
+
+#ifndef immigrants_h
+#define immigrants_h
+
+
+#endif /* immigrants_h */
